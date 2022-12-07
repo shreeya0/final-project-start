b521443375a1c5016ab5986a02299c6ae4b4e0e8
Daniel Yang <daniely@udel.edu>
2022-12-07 15:42:53 -0500

    Tests

--- src/Fish.test.tsx
+++ src/Fish.test.tsx
@@ -1,33 +1,55 @@
-import {
-    Goldfish,
-    Squid,
-    Beta,
-    Minnow,
-    Octopus,
-    Clownfish,
-    Shark,
-    Whale,
-    Dolphin,
-    Sponge,
-    Crab,
-    Tang,
-    JellyFishB,
-    JellyFishP,
-    Snail,
-    Snail2,
-    Koi,
-    Trout,
-    Catfish,
-    Seahorse,
-    Lionfish,
-    Orca,
-    Crocodile,
-    Eel,
-    Lobster,
-    Pufferfish,
-    Sardine,
-    Anglerfish,
-    Sunfish,
-    Swordfish,
-    Whaleshark
-} from "./interfaces/fishpics";
+import { screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import { Menu } from "./Menu";
+import NumSquareForm from "./NumSquareForm";
+import ReactDOM from "react-dom";
+import { observe } from "./game";
+import "./Container";
+import { Fish } from "./Fish";
+
+const TANKS = screen.getAllByTestId("tank");
+
+describe("Testing if Fishdom is rendering", () => {
+    const getFishdom = observe((picPosition: [number, number]) => {
+        ReactDOM.render(
+            <React.StrictMode>
+                <h1
+                    className="text-center"
+                    style={{
+                        fontStyle: "Bold",
+                        fontFamily: "Impact",
+                        fontSize: "80px"
+                    }}
+                >
+                    FISHDOM: Shreeya, Daniel, Jason
+                </h1>
+                <div>
+                    <Menu></Menu>
+                </div>
+                <div>
+                    <NumSquareForm picPosition={picPosition}></NumSquareForm>
+                </div>
+                <div className="App"></div>
+            </React.StrictMode>,
+            document.getElementById("root")
+        );
+    });
+    expect(getFishdom).toBeInTheDocument();
+});
+
+test("Testing if fish is in tank", () => {
+    const tank = screen.getByTestId("tank");
+    tank.click();
+    expect(Fish).toBeInTheDocument();
+});
+
+test("Testing filter button", () => {
+    const button = screen.getByText("Filter through Options");
+    button.click();
+    expect(screen.getByLabelText("Choose Fish Type")).toBeInTheDocument();
+});
+
+//test("Testing if button changed fish to place down", () => {});
+
+//test("Testing if fish moved successfully to another tank", () => {});
